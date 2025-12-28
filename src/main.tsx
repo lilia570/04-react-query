f0f70fdf4f11046a9c1f7210041d6a2bc9967d46
lilia570 <liliapaslavska570@gmail.com>
2025-12-28 16:03:49 +0200

    replace

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App/App"; 
+import App from "./components/App/App"; 
 import "modern-normalize/modern-normalize.css";
 import { Toaster } from "react-hot-toast";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
